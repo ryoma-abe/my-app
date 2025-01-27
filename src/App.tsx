99df6abf727d40c5a4cdda240e919859d5db2045
ryoma-abe <liaozhenanbu387@gmail.com>
2025-01-27 17:17:16 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { List } from "./component/List";
+
+const Example = () => {
+  return (
+    <div>
+      <h1 className="component">Example</h1>
+      <List />
+    </div>
+  );
+};
+
+export default Example;
